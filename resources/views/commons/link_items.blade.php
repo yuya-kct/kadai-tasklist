37831b9b7fa8a37bf5560debabbd937c336e2e5d
yuya.tsurusaki <yuya.tsurusaki@hakuhodody-one.co.jp>
2025-06-09 14:42:01 +0900

    top page

---
+++ resources/views/commons/link_items.blade.php
@@ -0,0 +1,15 @@
+@if (Auth::check())
+    {{-- ユーザー一覧ページへのリンク --}}
+    <li><a class="link link-hover" href="#">Users</a></li>
+    {{-- ユーザー詳細ページへのリンク --}}
+    <li><a class="link link-hover" href="#">{{ Auth::user()->name }}&#39;s profile</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- ログアウトへのリンク --}}
+    <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
+@else
+    {{-- ユーザー登録ページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('register') }}">Signup</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- ログインページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('login') }}">Login</a></li>
+@endif
